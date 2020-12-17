7faa8f7668d1ff7ef46df3a4fabd61afba4223c8
ryanmkump <kump.9@buckeyemail.osu.edu>
2020-12-16 21:38:16 -0500

    Setup Spring

---
+++ src/main/java/me/quinnkump/app/StockScraperApplication.java
@@ -0,0 +1,13 @@
+package me.quinnkump.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockScraperApplication.class, args);
+	}
+
+}
